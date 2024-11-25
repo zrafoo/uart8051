c6a0092e0d860bea7ed5b6c651f08a4d624e004e
Rostislav <71731666+zrafoo@users.noreply.github.com>
2024-11-25 16:14:57 +0300

    Add Qt files

---
+++ mainwindow.h
@@ -0,0 +1,58 @@
+
+
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QSerialPort>
+
+QT_BEGIN_NAMESPACE
+
+class QLabel;
+class QTimer;
+
+namespace Ui {
+class MainWindow;
+}
+
+QT_END_NAMESPACE
+
+class Console;
+class SettingsDialog;
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void openSerialPort();
+    void closeSerialPort();
+//    void about();
+    void writeData(const QByteArray &data);
+    void readData();
+
+    void handleError(QSerialPort::SerialPortError error);
+    void handleBytesWritten(qint64 bytes);
+    void handleWriteTimeout();
+
+private:
+    void initActionsConnections();
+
+private:
+    void showStatusMessage(const QString &message);
+    void showWriteError(const QString &message);
+
+    Ui::MainWindow *m_ui = nullptr;
+    QLabel *m_status = nullptr;
+    Console *m_console = nullptr;
+    SettingsDialog *m_settings = nullptr;
+    qint64 m_bytesToWrite = 0;
+    QTimer *m_timer = nullptr;
+    QSerialPort *m_serial = nullptr;
+};
+
+#endif // MAINWINDOW_H
